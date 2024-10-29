29d222418af9ba7fdf13f9b3764037717e0c956f
`Naufal-Pasya` <c038dsx1987@bangkit.academy>
2024-10-29 10:01:44 +0700

    vercel

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
